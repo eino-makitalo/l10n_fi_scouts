ccc2e095d2a1bdc90e0b2a8e3fa825e95d95d557
eino <eino.makitalo@netitbe.fi>
2016-02-14 10:46:48 +0200

    inc vers

--- __openerp__.py
+++ __openerp__.py
@@ -1,9 +1,9 @@
-# -*- encoding: utf-8 -*-
+﻿# -*- encoding: utf-8 -*-
 # (C) Eino Mäkitalo, Aviapartio ry.  MIT License or LGPL if you like.
 # Based on voluntary work.
 {
     'name': 'Finnish template for local scout groups',
-    'version': '0.1.4',
+    'version': '0.1.5',
     'author': 'Aviapartio ry. / Eino Mäkitalo',
     'category': 'Localization/Account Charts',
     'description': """
